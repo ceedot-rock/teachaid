ca72e84d9607c2162308d5f5a4b463d4e493a074
Corey Tasz <ceedotrock@gmail.com>
2026-07-29 02:09:45 -0400

    Fix service worker asset list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'teachaid-v1';
+const CACHE = 'teachaid-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
